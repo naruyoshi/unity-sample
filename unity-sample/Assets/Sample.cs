2941f96926208f7cfffab33bbc3f3b10a7203271
naruyoshi <ttktkktktk@gmail.com>
2017-10-09 00:10:44 +0900

    10/9

--- unity-sample/Assets/Sample.cs
+++ unity-sample/Assets/Sample.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
 
+// sample
 public class Sample : MonoBehaviour {
 
+	Rigidbody2D rigid2D;
+	float count;
+	string dateTimeStr;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +16,24 @@ public class Sample : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// 日時情報取得
+		//dateTimeStr = System.DateTime.Now.ToString ();
+		//Debug.Log (dateTimeStr);
+
+		// time.deltatime sample
+		// time.deltaTimeで1フレーム辺りの時間を取得
+		count += Time.deltaTime;
+		//Debug.Log (count);
+
+		// 1secをカウント
+		if (count >= 1.0f) {
+			dateTimeStr = System.DateTime.Now.ToString ();
+			Debug.Log (dateTimeStr);
+			Debug.Log ("1sec" + count);
+			count = 0.0f;
+		}
+
+
+
 	}
 }
